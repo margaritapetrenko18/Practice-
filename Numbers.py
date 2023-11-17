4903924bff52447716ef4bf15e057c413438d4bf
margaritapetrenko18 <margomorel18@gmail.com>
2023-11-17 14:26:42 +0100

    Numbers

---
+++ Numbers.py
@@ -0,0 +1,21 @@
+print("hello Margo!")
+def add_numbers(a, b):
+    """
+    This function takes two numbers as input and returns their sum.
+
+    Parameters:
+    - a: first number
+    - b: second number
+
+    Returns:
+    - sum of a and b
+    """
+    result = a + b
+    return result
+
+# Example usage:
+num1 = 5
+num2 = 7
+sum_result = add_numbers(num1, num2)
+
+print(f"The sum of {num1} and {num2} is: {sum_result}")
